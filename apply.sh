c1d42a1320e7f628f540e1764025907d049f2825
llenax <67596732+llenax@users.noreply.github.com>
2025-04-17 18:25:03 +0300

    feat: config files sync script

---
+++ apply.sh
@@ -0,0 +1,52 @@
+# clear the shell's hash table
+hash -r
+
+CONFIG_DIR=$HOME/.config
+SSH_PUBKEY_FILE=$HOME/.ssh/id_ed25519.pub
+SSH_ALLOWED_SIGNERS_FILE=$CONFIG_DIR/git/allowed_signers
+
+require_apt_pkg() {
+    package_apt=$1
+    package_name=$2
+
+    if [ -z "$package_name" ]; then
+	package_name=$package_apt
+    fi
+
+    echo "checking for: $package_name"
+
+    if [ -f "$(command -v $package_name)" ]; then
+	echo "found: $(command -v $package_name)"
+    else
+	echo "$package_name not found. Attempting to install..."
+	if sudo apt install -y $package_apt; then
+	    echo "$package_name installed successfully"
+	else
+	    echo "failed to install $package_name"
+	fi
+    fi
+
+    echo ----------------------
+}
+
+
+for dir in ./*; do
+    if [ -d $dir ]; then
+	remote=$CONFIG_DIR/${dir:2}
+	
+	rm -rf $remote
+	mkdir -p $remote
+	cp -r "$dir"/* "$remote"
+    fi
+done
+
+require_apt_pkg "git"
+require_apt_pkg "gnupg2" "gpg2"
+
+(
+    email=$(git config --get user.email)
+    ns="namespaces=\"git\""
+    pubkey=$(cat $SSH_PUBKEY_FILE)
+    echo "$email $ns $pubkey" >> $SSH_ALLOWED_SIGNERS_FILE
+)
+
